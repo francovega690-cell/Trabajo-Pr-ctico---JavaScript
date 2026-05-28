a9f6fbcdf5741a00da9fc2b438f4d6f44510e410
francovega690-cell <francovega690@gmail.com>
2026-05-28 07:18:35 -0300

    punto5

--- node.js
+++ node.js
@@ -1,5 +1,7 @@
-let numero = 2;
+let suma = 0;
 
-for (let i = 0; i <= 10; i++) {
-  console.log(numero + "x" + i + "=" + numero * i);
+for (let i = 1; i <= 100; i++) {
+  suma += i;
 }
+
+console.log("La suma es: " + suma);
